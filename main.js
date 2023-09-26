5da1e9af8369c29dec1c47227b3cda57243f2174
JerryUusis <jerry.uusitalo@gmail.com>
2023-09-26 14:30:00 +0300

    Finished working with the MVP

--- main.js
+++ main.js
@@ -5,32 +5,76 @@ const endButton = document.querySelector("#end-button");
 const circles = document.querySelectorAll(".circle");
 const scoreDisplay = document.querySelector(".score");
 let score = 0;
+let timer;
+let timerSpeed = 1000;
+let activeNumber = 0;
+let rounds = 0;
+
+// Functions
+
+const getRandomNumber = (min, max) =>
+    Math.floor(Math.random() * (max - min + 1)) + min;
+
+function clickCircle(i) {
+    if (i !== activeNumber) {
+        return endGame();
+    }
+    rounds--;
+    score += 1;
+    scoreDisplay.textContent = score;
+}
 
 circles.forEach((circle, i) => {
     circle.addEventListener("click", () => clickCircle(i));
 })
 
-// Functions
+function enableEvents() {
+    circles.forEach(circle => {
+        circle.style.pointerEvents = "auto";
+    })
+}
 
 function startGame() {
-    console.log("Game started");
+    if (rounds >= 3) {
+        return endGame();
+    }
+    
+    enableEvents();
+    // Sets the active circle as a random number
+    const newActive = pickNewNumber(activeNumber);
+
+    // Adds .active for .circle with the active number and takes active class away from previous active number
+    circles[newActive].classList.toggle("active");
+    circles[activeNumber].classList.remove("active");
+
+    // Resets the active number using pickNewNumber function
+    activeNumber = newActive;
+
+    timer = setTimeout(startGame, timerSpeed);
+
+    timerSpeed -= 10;
+    rounds++;
+
+    function pickNewNumber(activeNumber) {
+        const newActive = getRandomNumber(0, 3);
+
+        if (newActive !== activeNumber) {
+            console.log(newActive);
+            return newActive;
+        } else {
+            return pickNewNumber(activeNumber)
+        }
+    }
 }
 
 function endGame() {
-    console.log("Game ended");
+    clearTimeout(timer);
+    resetGame();
 }
 
-function clickCircle(i) {
-    console.log("Circle was clicked " + i);
-    score += 1;
-    scoreDisplay.textContent = score;
+function resetGame() {
+    window.location.reload();
 }
-
-const getRandomNumber = (min, max) => {
-    Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-console.log(getRandomNumber(1,4));
 
 // Event listeners
 
